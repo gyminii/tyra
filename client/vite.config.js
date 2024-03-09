462a8520e6c171ba930b5f17ba7d029fcd64d370
Gyeong-Min Lee <56126107+gyminii@users.noreply.github.com>
2024-03-09 13:58:07 -0800

    Script changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,9 +5,4 @@ import graphqlLoader from "vite-plugin-graphql-loader";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), graphqlLoader()],
-	base: "/",
-	build: {
-		commonjsOptions: { exclude: ["shared-project"] }, // <----
-		// commonjsOptions: { include: [] },                                          // Edit:
-	},
 });
