c0f5f70bfa26344d238b963762d7f3840dad6bca
Gyeong-Min Lee <56126107+gyminii@users.noreply.github.com>
2024-03-08 23:44:50 -0800

    PORT

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import graphqlLoader from "vite-plugin-graphql-loader";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), graphqlLoader()],
+	server: { port: 5555 },
 	build: {
 		commonjsOptions: { exclude: ["shared-project"] }, // <----
 		// commonjsOptions: { include: [] },                                          // Edit:
