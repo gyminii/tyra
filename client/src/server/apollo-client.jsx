c5719d4b1b02733f5bce38386ad82b88c4125bb0
Gyeong-Min Lee <56126107+gyminii@users.noreply.github.com>
2024-03-09 22:31:46 -0800

    URI change

--- client/src/server/apollo-client.jsx
+++ client/src/server/apollo-client.jsx
@@ -7,7 +7,7 @@ import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
 export const ApolloProvider = ({ children }) => {
 	const graphqlUri =
 		process.env.NODE_ENV === "production"
-			? "https://tyra-production.up.railway.app/" // Production URI
+			? "https://tyra.up.railway.app/" // Production URI
 			: "http://localhost:5000/";
 	const client = new ApolloClient({
 		uri: graphqlUri,
